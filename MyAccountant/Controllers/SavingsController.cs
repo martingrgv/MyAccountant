2c228f4946cc7cb8a8a16c120e160aaffd62a529
martingrgv <martin.geor04@gmail.com>
2024-06-12 17:58:34 +0300

    Add project files.

---
+++ MyAccountant/Controllers/SavingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAccountant.Controllers
+{
+    public class SavingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
